8d9688f99e8f466acfae75db0fb8ae430cfa50c4
Mary Moorhead <marycm@live.co.uk>
2022-08-21 23:07:06 +0100

    buttons work now and tried to add calculate function

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ const key = e.target;
 const action = key.dataset.action;
 
 if (!action) {
-  console.log('number key')
+  console.log('number key');
 }
 
 if (
@@ -18,20 +18,98 @@ if (
   action === 'multiply' ||
   action === 'divide'
 ) {
-  console.log('operator key!')
+  console.log('operator key!');
+  calculator.dataset.previousKeyType = 'operator';
+  
+  const previousKeyType = calculator.dataset.previousKeyType;
+
+  if (!action) {
+    if (displayedNum === '0' || previousKeyType === 'operator') {
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayedNum + keyContent;
+    }
+  }
+
 }
 
 if (action === 'decimal') {
-  console.log('decimal key!')
+  console.log('decimal key!');
 }
 
 if (action === 'clear') {
-  console.log('clear key!')
+  console.log('clear key!');
 }
 
 if (action === 'calculate') {
-  console.log('equal key!')
+  console.log('equal key!');
 }
 
 }
 });
+
+// clicking number keys
+const display = document.querySelector('.calculator__display');
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    
+
+    if (!action) {
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
+    }
+    
+
+  }
+})
+
+// clicking the equals sign
+if (action === 'calculate') {
+  const firstValue = calculator.dataset.firstValue;
+  const operator = calculator.dataset.operator;
+  const secondValue = displayedNum;
+  
+  display.textContent = calculate(firstValue, operator, secondValue);
+
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    // ...
+    calculator.dataset.firstValue = displayedNum;
+    calculator.dataset.operator = action;
+  }
+
+  // calculate function
+  const calculate = (n1, operator, n2) => {
+    let result = '';
+  
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2);
+    }
+  
+    return result;
+  }
+
+
+}
--- dist/calculator.dev.js
+++ dist/calculator.dev.js
@@ -6,26 +6,88 @@ keys.addEventListener('click', function (e) {
   if (e.target.matches('button')) {
     // do something
     var key = e.target;
-    var action = key.dataset.action;
+    var _action = key.dataset.action;
 
-    if (!action) {
+    if (!_action) {
       console.log('number key');
     }
 
-    if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+    if (_action === 'add' || _action === 'subtract' || _action === 'multiply' || _action === 'divide') {
       console.log('operator key!');
+      calculator.dataset.previousKeyType = 'operator';
+      var previousKeyType = calculator.dataset.previousKeyType;
+
+      if (!_action) {
+        if (displayedNum === '0' || previousKeyType === 'operator') {
+          display.textContent = keyContent;
+        } else {
+          display.textContent = displayedNum + keyContent;
+        }
+      }
     }
 
-    if (action === 'decimal') {
+    if (_action === 'decimal') {
       console.log('decimal key!');
     }
 
-    if (action === 'clear') {
+    if (_action === 'clear') {
       console.log('clear key!');
     }
 
-    if (action === 'calculate') {
+    if (_action === 'calculate') {
       console.log('equal key!');
     }
   }
-});
+}); // clicking number keys
+
+var display = document.querySelector('.calculator__display');
+keys.addEventListener('click', function (e) {
+  if (e.target.matches('button')) {
+    var key = e.target;
+    var _action2 = key.dataset.action;
+    var _keyContent = key.textContent;
+    var _displayedNum = display.textContent;
+
+    if (!_action2) {
+      if (_displayedNum === '0') {
+        display.textContent = _keyContent;
+      } else {
+        display.textContent = _displayedNum + _keyContent;
+      }
+    }
+
+    if (_action2 === 'decimal') {
+      display.textContent = _displayedNum + '.';
+    }
+  }
+}); // clicking the equals sign
+
+if (action === 'calculate') {
+  var firstValue = calculator.dataset.firstValue;
+  var operator = calculator.dataset.operator;
+  var secondValue = displayedNum;
+  display.textContent = calculate(firstValue, operator, secondValue);
+
+  if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+    // ...
+    calculator.dataset.firstValue = displayedNum;
+    calculator.dataset.operator = action;
+  } // calculate function
+
+
+  var calculate = function calculate(n1, operator, n2) {
+    var result = '';
+
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2);
+    }
+
+    return result;
+  };
+}
